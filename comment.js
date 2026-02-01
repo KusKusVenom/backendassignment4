2ebf20789a8d03ae9f1e4f509988635a3f4e1a96
KusKusVenom <berkanat07@gmail.com>
2026-02-01 16:36:48 +0500

    comment.js

---
+++ comment.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: [true, 'Please provide comment content'],
+    trim: true,
+    minlength: [1, 'Comment must be at least 1 character'],
+    maxlength: [1000, 'Comment cannot exceed 1000 characters']
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: [true, 'Comment must belong to a post']
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Update the updatedAt field on save
+commentSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
